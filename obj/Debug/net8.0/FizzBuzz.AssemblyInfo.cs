0684dc85323449677fec893be4bcbe5c7b1c6f7c
Hannah Osei <hannah.osei@softwire.com>
2025-05-27 11:28:09 +0100

    prints 'Buzz' whenever the number is a multiple of 5, and 'FizzBuzz' whenever the number is a multiple of 3 and 5

--- obj/Debug/net8.0/FizzBuzz.AssemblyInfo.cs
+++ obj/Debug/net8.0/FizzBuzz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad9b0c7177ec3d7c0e5858102ddf52b5753ce309")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58333b626eae2ade59b1cce7796ff2ea10691ed0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
