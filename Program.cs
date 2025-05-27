7166fa9ca105f6eaaee4e861825eb84577353d88
Hannah Osei <hannah.osei@softwire.com>
2025-05-27 11:04:36 +0100

    prints every number from 1 to 100

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int number = 1;
+while (number < 100) {
+    number++;
+    Console.WriteLine(number);   
+}
